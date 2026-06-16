16553ed9da05efa64bd134cc425b3ff85ab36ed1
Andvrew <seyiababs@gmail.com>
2026-06-16 16:58:32 -0400

    figure out what needs to be made and implemented

---
+++ KTPLIFE/KTPLIFE/KTPLIFEApp.swift
@@ -0,0 +1,32 @@
+//
+//  KTPLIFEApp.swift
+//  KTPLIFE
+//
+//  Created by Seyi Babatunde on 6/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KTPLIFEApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ KTPLIFE/KTPLIFETests/KTPLIFETests.swift
@@ -0,0 +1,17 @@
+//
+//  KTPLIFETests.swift
+//  KTPLIFETests
+//
+//  Created by Seyi Babatunde on 6/16/26.
+//
+
+import Testing
+@testable import KTPLIFE
+
+struct KTPLIFETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
