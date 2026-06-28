c1b3f9241ba7a90cf31aa431d9bf5644a63048ae
yrv70931 <yrv70931@uga.edu>
2026-06-28 12:55:35 -0400

    Schema and seed structure

--- ktp-api/db/seed.sql
+++ ktp-api/db/seed.sql
@@ -1,7 +1,3 @@
-INSERT INTO members (name, role, year, member_group) VALUES
-  ('Andrew Babatunde', 'Computer Science', '2027', 'activeMembers'),
-  ('Danny Rifai', 'Data Science Track', '2026', 'activeMembers');
-
 INSERT INTO events (title, start_date, end_date, description) VALUES
   (
     'Chapter Meeting',
